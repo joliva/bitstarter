692881091c102e1a5760d250bfebb6ffe113f36c
joliva <joliva63@gmail.com>
2013-07-10 16:39:42 +0000

    Added ability to accept a URL as an HTML source in addition to existing capability for files.

--- grader.js
+++ grader.js
@@ -24,51 +24,86 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
-    var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-    }
-    return instr;
-};
-
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+	var instr = infile.toString();
+	if(!fs.existsSync(instr)) {
+	    console.log("%s does not exist. Exiting.", instr);
+	    process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	}
+	return instr;
 };
 
 var loadChecks = function(checksfile) {
-    return JSON.parse(fs.readFileSync(checksfile));
+	return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
-    }
-    return out;
+var checkHtml = function(cheerio_object, checksfile) {
+	$ = cheerio_object
+	var checks = loadChecks(checksfile).sort();
+	var out = {};
+	for(var ii in checks) {
+	    var present = $(checks[ii]).length > 0;
+	    out[checks[ii]] = present;
+	}
+	return out;
+};
+
+var checkHtmlFile = function(htmlfile, checksfile, callback) {
+	fs.readFile(htmlfile, function(err, data) {
+		if (err) {
+			console.error('Error loading file: ' + htmlfile);
+		} else {
+			var cheerio_object = cheerio.load(data);
+			callback(checkHtml(cheerio_object, checksfile));
+		}
+	});
+};
+
+var checkHtmlUrl = function(url, checksfile, callback) {
+	rest.get(url)
+		.on('success', function(result, response) {
+			var cheerio_object = cheerio.load(result);
+			callback(checkHtml(cheerio_object, checksfile));
+		})
+		.on('fail', function(data, response) {
+			console.error('Failure loading URL: ' + url);
+		})
+		.on('error', function(err, response) {
+			console.error('Error loading URL: ' + url);
+		});
 };
 
 var clone = function(fn) {
-    // Workaround for commander.js issue.
-    // http://stackoverflow.com/a/6772648
-    return fn.bind({});
+	// Workaround for commander.js issue.
+	// http://stackoverflow.com/a/6772648
+	return fn.bind({});
+};
+
+var outputJson = function(json) {
+	var outJson = JSON.stringify(json, null, 4);
+	console.log(outJson);
 };
 
 if(require.main == module) {
-    program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+	program
+	    .option('-c, --checks <check_file>', 'Path to checks file', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	    .option('-f, --file <html_file>', 'Path to HTML file', HTMLFILE_DEFAULT)
+	    .option('-u, --url <webpage_url>', 'URL for webpage to check')
+	    .parse(process.argv);
+
+	// accept either URL or file path, with url having precedence
+	// note: outputJson is a callback function 
+	if (program.url !== undefined) {
+		checkHtmlUrl(program.url, program.checks, outputJson);
+	} else {
+		checkHtmlFile(program.file, program.checks, outputJson);
+	}
 } else {
-    exports.checkHtmlFile = checkHtmlFile;
+	exports.checkHtmlFile = checkHtmlFile;
+	exports.checkHtmlUrl = checkHtmlUrl;
 }
