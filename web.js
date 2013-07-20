94532ee1912e8f5dd982db8f88c0b1cc533ab068
John Oliva <joliva63@gmail.com>
2013-07-19 22:02:05 -0400

    Add images and ability to serve them for the page

--- web.js
+++ web.js
@@ -8,6 +8,12 @@ app.get('/', function(request, response) {
 	response.send(buf.toString());
 });
 
+app.get('/images/:image_file', function(request, response) {
+	var buf = fs.readFileSync('images/' + request.params.image_file);
+	response.writeHead(200, {'Content-Type': 'image/jpeg'});
+	response.end(buf,'binary');
+});
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
